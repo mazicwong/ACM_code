27f8bff9ea61fc34bcc8d2a9ac3ff5a8ac1c5a96
mazicwong <mazicwong@gmail.com>
2017-07-21 22:28:14 +0800

    .

---
+++ code/graph/最短路/poj1502.cpp
@@ -0,0 +1,24 @@
+/*************************************************************************
+	> File Name: poj1502.cpp
+	> Author: mazicwong
+	> Mail: mazicwong@gmail.com
+	> Created Time: 2017年07月21日 星期五 13时31分03秒
+ ************************************************************************/
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    freopen("in","r",stdin);
+    {
+        
+    }
+    
+    return 0;
+}
